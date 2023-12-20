f752804c5a894c390a2688da01c2ec327bfc35f6
Elene Dgebuadze <elenedgebuadze83@gmail.com>
2023-12-21 01:12:47 +0400

    Fetched and styled the category filter buttons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/navbar/navbar.js";
 import Header from "./components/header/header.js";
+import Categories from "./components/categories/categories.js";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Header />;
+      <Categories />
     </div>
   );
 }
---
+++ src/components/categories/categories.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+import "./categories.css";
+
+const Categories = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.blog.redberryinternship.ge/api/categories")
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+        setCategories(data.data);
+      });
+  }, []);
+
+  return (
+    <div className="categoryButtons">
+      {categories.map((category) => (
+        <button
+          style={{
+            color: category.text_color,
+            backgroundColor: category.background_color,
+            fontSize: "12px",
+            fontFamily: "firaGo",
+            borderRadius: "30px",
+            padding: "8px 16px",
+            border: "none",
+            cursor: "pointer",
+          }}
+          key={category.id}
+        >
+          {category.title}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default Categories;
