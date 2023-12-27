c3fc263f25e5065e8792dfc7cec617bc72f34a0c
Elene Dgebuadze <elenedgebuadze83@gmail.com>
2023-12-27 17:44:22 +0400

    Added upload image functionality

--- src/pages/createBlog.js
+++ src/pages/createBlog.js
@@ -1,28 +1,56 @@
 import "./createBlog.css";
-import Logo from "../components//navbar/logo.png";
+import Logo from "../components/navbar/logo.png";
 import BackArrow from "./backArrow.png";
 import { useState } from "react";
+import InputIcon from "./upload.png";
 
 const Create = () => {
   const [uploadedFile, setUploadedFile] = useState(null);
+  const [fileUploaded, setFileUploaded] = useState(false);
 
   const change = (e) => {
     const file = e.target.files[0];
     if (file && file.type.includes("image")) {
       setUploadedFile(file);
+      setFileUploaded(true);
     } else {
       setUploadedFile(null);
-      //aq errori pls select an image
-      console.log("select an image");
+      setFileUploaded(false);
     }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
   };
 
   return (
     <div className="zhuzhu">
-      <img src={Logo} className="logo" alt="Redberry Logo"></img>
+      <img src={Logo} className="logo" alt="Redberry Logo" />
       <img src={BackArrow} className="back-arrow" alt="back-arrow" />
       <h1 className="form-name">ბლოგის დამატება</h1>
-      <form></form>
+      <h1 className="custom-upload-text">ატვირთეთ ფოტო</h1>
+      <label htmlFor="image-input" className="custom-upload">
+        <span>
+          {!fileUploaded && (
+            <img className="input-icon" src={InputIcon} alt="Upload Icon" />
+          )}
+          {fileUploaded ? (
+            <span style={{ marginLeft: "10px" }}>{uploadedFile.name} </span>
+          ) : (
+            <>
+              ჩააგდეთ ფაილი აქ ან{" "}
+              <span style={{ textDecoration: "underline" }}>აირჩიეთ ფაილი</span>
+            </>
+          )}
+        </span>
+        <input
+          id="image-input"
+          type="file"
+          accept="image/*"
+          onChange={change}
+        />
+      </label>
+      <button onClick={handleSubmit}>Submit</button>
     </div>
   );
 };
